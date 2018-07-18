d2369f3e1e557bfc2b5eb666dfcab99d8e0b4516
JuanCruzMendez <juancm91@gmail.com>
2018-07-18 22:03:08 +0100

    olaqase

--- todo-front/src/components/TodoList.vue
+++ todo-front/src/components/TodoList.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="container">
-    <input type="text" v-model="newTodo.text"  @keyup.enter="crearTodo()"> <button @click="crearTodo">Añadir todo</button>
+    <input type="text" v-model="newTodo.text" v-on:keyup.enter="crearTodo"> 
+    <button @click="crearTodo">Añadir todo</button>
 
       <ul>
           <li v-for="todo in todos" :key="todo.id">
            El texto es {{todo.text}} y fue creado {{todo.createdAt}}
+          <button @click="editarTodo" v-bind="todo.id">Editar todo</button>
+          <button @click="borrarTodo" v-bind="todo.id">Borrar todo</button>
+
           </li>
 
       </ul>
@@ -16,42 +20,59 @@
 </template>
 
 <script>
-import axios from "axios"
+import axios from "axios";
 
 export default {
   name: "todo-list",
+
   data() {
     return {
       todos: [],
       newTodo: {
         text: "",
         createdAt: 11,
-        iscompleted: true
+        isCompleted: true
       }
     };
   },
 
   methods: {
     recargarTodos() {
-      axios.get("http://localhost:2222/api/todos")
+      axios
+        .get("http://localhost:2222/api/todos")
         .then(response => (this.todos = response.data));
     },
 
-    crearTodo(){
-      axios.post("http://localhost:2222/api/todos", this.newTodo.text)
-        .then(response => (this.todos.push(response.data)));
+    crearTodo() {
+      axios.post("http://localhost:2222/api/todos", this.newTodo)
+        .then(response => this.todos.push(response.data));
+
+      },
+        
+    editarTodo(){
+      axios.patch("http://localhost:2222/api/todos/"+this.todo.id, this.newTodo.text)
+      .then(response => this.todos.findIndex(idx => idx.id === response.data.id))
       
+    },
+
+    borrarTodo(){
+      axios.delete("http://localhost:2222/api/todos/"+this.todo.id, this.newTodo.text)      
     }
+  },
+  
 
-
+  created() {
+    this.recargarTodos()
   },
 
-  created(){      
-    this.recargarTodos(),
-    this.crearTodo()
+
+  computed: {
+    totalPrice() {
+      let result = 0;
+      this.items.forEach(item => (result += item.price));
+      return result;
+    }
   }
-
-
 };
 </script>
 
